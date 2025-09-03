64dc0b4fcb627ff708942700f50cb77ec93da0b2
Guilherme Carvalho <162490839+guicarbar@users.noreply.github.com>
2025-09-02 22:53:05 -0300

    Update - footer (#3)

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,13 +1,26 @@
 function menu() {
-  // get elements
-  const navlink = document.getElementById("navbarList")
+  // vars
+  const menuList = document.getElementById('navbarList')
+  const menuBtn = document.getElementById('navbarBtn')
 
-  // verify display mode
-  if (navlink.style.display == "none") {
-    // show menu links
-    navlink.style.display = "inline-block"
+  // menuList
+  if (menuList.classList.contains('hidden')) {
+    // toggle icon for btn
+    menuBtn.classList.remove('bi-list')
+    menuBtn.classList.add('bi-x-lg')
+    // toggle display menu
+    menuList.classList.remove('hidden')
+    menuList.classList.add('show')
   } else {
-    // hode navbar link
-    navlink.style.display = "none"
+    // toggle icon for btn
+    menuBtn.classList.remove('bi-x-lg')
+    menuBtn.classList.add('bi-list')
+    // toggle display menu
+    menuList.classList.remove('show')
+    menuList.classList.add('closeMenu')
+    setTimeout(() => {
+      menuList.classList.remove("closeMenu");
+      menuList.classList.add("hidden");
+    }, 400)
   }
 }
