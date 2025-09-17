234570143f6a994be6a97aa7fcdd56b5190ac75a
Guilherme Carvalho <162490839+guicarbar@users.noreply.github.com>
2025-09-16 22:20:47 -0300

    update - final mobile (#10)

--- javascript/value.js
+++ javascript/value.js
@@ -1,7 +1,27 @@
-function changeValue(value) {
-  if (value === 1) {
-    // anual
-  } if (value ===-1) {
-    // mensal
+function changeValue() {
+  // element
+  const elementIndicator = document.getElementById('indicator')
+  const popularValue = document.getElementById('popularValue')
+  const ultraValue = document.getElementById('ultraValue')
+
+  // check
+  if (elementIndicator.classList.contains('yearBtn')) {
+    // MM
+    // change class
+    elementIndicator.classList.remove('yearBtn')
+    elementIndicator.classList.add('mouthBtn')
+    
+    //change values in elements
+    popularValue.textContent = 'R$20'
+    ultraValue.textContent = 'R$200'
+  } else if (elementIndicator.classList.contains('mouthBtn')) {
+    // yy
+    // change class
+    elementIndicator.classList.remove('mouthBtn')
+    elementIndicator.classList.add('yearBtn')
+
+    //change values in elements
+    popularValue.textContent = 'R$16'
+    ultraValue.textContent = 'R$160'
   }
-} 
+}
