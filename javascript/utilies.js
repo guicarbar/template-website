8a301115fc07514d1f1fa730fd8efb0b11a19e4b
Guilherme Carvalho <162490839+guicarbar@users.noreply.github.com>
2025-09-21 16:27:07 -0300

    update - finalized structure (#12)

--- javascript/utilies.js
+++ javascript/utilies.js
@@ -4,3 +4,16 @@ document.addEventListener("resize", () => {
   clearTimeout(timeout)
   timeout = setTimeout(() => { location.reload() }, 300)
 })
+
+
+// navbar media querrie
+window.addEventListener("resize", () => {
+  
+})
+
+// navbar media querrie (initial load)
+if (window.innerWidth >= 768) {
+  document.querySelector('.navbar-link')?.classList.remove('hidden');
+} else {
+  document.querySelector('.navbar-link')?.classList.add('hidden');
+}
